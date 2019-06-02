83d8f4faa41f37cf9abf12620df68713985b08b4
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2019-06-02 14:29:46 -0300

    Primer Commit

---
+++ base_de_datos.sql
@@ -0,0 +1,78 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.37-MariaDB : Database - prog2dao1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prog2dao1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `prog2dao1`;
+
+/*Table structure for table `persona` */
+
+DROP TABLE IF EXISTS `persona`;
+
+CREATE TABLE `persona` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) DEFAULT NULL,
+  `apellido` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `persona` */
+
+insert  into `persona`(`id`,`nombre`,`apellido`) values (2,'Martin','Ortiz'),(3,'Juan','Perez'),(4,'Laura','Lopez');
+
+/*Table structure for table `productos` */
+
+DROP TABLE IF EXISTS `productos`;
+
+CREATE TABLE `productos` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `productos` */
+
+/*Table structure for table `relventaproducto` */
+
+DROP TABLE IF EXISTS `relventaproducto`;
+
+CREATE TABLE `relventaproducto` (
+  `idventa` int(10) DEFAULT NULL,
+  `idproducto` int(10) DEFAULT NULL,
+  KEY `relventa` (`idventa`),
+  KEY `relproducto` (`idproducto`),
+  CONSTRAINT `relproducto` FOREIGN KEY (`idproducto`) REFERENCES `productos` (`id`),
+  CONSTRAINT `relventa` FOREIGN KEY (`idventa`) REFERENCES `ventas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `relventaproducto` */
+
+/*Table structure for table `ventas` */
+
+DROP TABLE IF EXISTS `ventas`;
+
+CREATE TABLE `ventas` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `idpersona` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `relpersonaventa` (`idpersona`),
+  CONSTRAINT `relpersonaventa` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ventas` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
