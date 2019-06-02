57fdac89a0da80a22141371779209efd29a93b2e
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2019-06-02 19:52:04 -0300

    terminado

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -25,11 +25,11 @@ CREATE TABLE `persona` (
   `nombre` varchar(30) DEFAULT NULL,
   `apellido` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 /*Data for the table `persona` */
 
-insert  into `persona`(`id`,`nombre`,`apellido`) values (2,'Martin','Ortiz'),(3,'Juan','Perez'),(4,'Laura','Lopez');
+insert  into `persona`(`id`,`nombre`,`apellido`) values (2,'Martin','Ortiz'),(3,'Juan','Perez'),(4,'Laura','Lopez'),(16,'Mateo','Morgui');
 
 /*Table structure for table `productos` */
 
@@ -38,10 +38,14 @@ DROP TABLE IF EXISTS `productos`;
 CREATE TABLE `productos` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) DEFAULT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `precio` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 /*Data for the table `productos` */
+
+insert  into `productos`(`id`,`nombre`,`descripcion`,`precio`) values (8,'pera','sonperas',50),(9,'naranja','sonnaranjas',60),(10,'carne','escarne',300),(11,'almohada','almo',2),(16,'ps4','play 4',5000);
 
 /*Table structure for table `relventaproducto` */
 
@@ -58,6 +62,8 @@ CREATE TABLE `relventaproducto` (
 
 /*Data for the table `relventaproducto` */
 
+insert  into `relventaproducto`(`idventa`,`idproducto`) values (8,8),(8,10),(8,16);
+
 /*Table structure for table `ventas` */
 
 DROP TABLE IF EXISTS `ventas`;
@@ -65,12 +71,17 @@ DROP TABLE IF EXISTS `ventas`;
 CREATE TABLE `ventas` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `idpersona` int(10) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `valor` int(10) DEFAULT NULL,
+  `descripcion` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `relpersonaventa` (`idpersona`),
   CONSTRAINT `relpersonaventa` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `ventas` */
+
+insert  into `ventas`(`id`,`idpersona`,`fecha`,`valor`,`descripcion`) values (8,16,'2019-06-02',5350,'mateo');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
